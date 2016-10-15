37330f40567c91141a90c161ffce756792b83536
Forward Creative Media <Forward Creative Media>
2016-10-15 14:29:19 -0400

    finishing up home page

--- wp-content/themes/stat-basketball/footer.php
+++ wp-content/themes/stat-basketball/footer.php
@@ -14,12 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer page-wrapper" role="contentinfo ">
-		<div class="site-info">
-		Christian Bryant Productions.
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'stat-basketball' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'stat-basketball' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'stat-basketball' ), 'stat-basketball', '<a href="http://underscores.me/" rel="designer">Christian Bryant</a>' ); ?>
-		</div><!-- .site-info -->
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
@@ -29,6 +24,17 @@
 
 <script src="http://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+<script src="./wp-content/themes/stat-basketball/js/scrollax.js"></script>
 <script src="./wp-content/themes/stat-basketball/js/app.js"></script>
+<script>
+$(document).ready(function(){
 
+
+$(window).enllax();
+
+
+
+})
+
+</script>
 </html>
--- wp-content/themes/stat-basketball/front-page.php
+++ wp-content/themes/stat-basketball/front-page.php
@@ -15,20 +15,119 @@
 
 <div class="page-wrapper">
 
-
-<div class="row">
-	<div class="col s12">
-		<div class="home-page--hero">
-					<img class="jumbo-image" src="./wp-content/themes/stat-basketball/img/StatBasketball.png">
-					<img class="quote" src="./wp-content/themes/stat-basketball/img/quote.png">
-					<div class="headline-container">
-							<h1 >IF YOU WANT TO GO FAST GO ALONE, <BR> IF YOU WANT TO GO FAR GO TOGETHER.</h1>
-							<hr class='stat-hr'>
-							<a href="#">LEARN MORE</a>
-					</div>
+<section id="homepage--header">
+	<div class="row">
+		<div class="col s12">
+			<div class="home-page--hero">
+						<img class="jumbo-image" src="./wp-content/themes/stat-basketball/img/StatBasketball.png">
+						<img class="quote" src="./wp-content/themes/stat-basketball/img/quote.png">
+						<div class="headline-container">
+								<h1 >IF YOU WANT TO GO FAST GO ALONE, IF YOU WANT TO GO FAR GO TOGETHER.</h1>
+								<hr class='stat-hr'>
+									<a href="#" class="btn stat-btn--orange">LEARN MORE <i class="material-icons right">arrow_forward</i></a>
+						</div>
+			</div>
 		</div>
 	</div>
-</div>
+</section>
+
+
+<section id="homepage--aboutus">
+
+		<img data-enllax-ratio=".09" data-enllax-type="foreground" src="./wp-content/themes/stat-basketball/img/Servitude.png">
+		<img data-enllax-ratio="-0.04" data-enllax-type="foreground" src="./wp-content/themes/stat-basketball/img/Accept_Responsibility.png">
+		<img data-enllax-ratio=".18" data-enllax-type="foreground" src="./wp-content/themes/stat-basketball/img/BeHumble.png">
+		<img data-enllax-ratio=".35" data-enllax-type="foreground" src="./wp-content/themes/stat-basketball/img/Embrace_Drive_Change.png">
+		<img data-enllax-ratio="-.04" data-enllax-type="foreground" src="./wp-content/themes/stat-basketball/img/Pursue_Growth.png">
+
+	<div class="row">
+		<div class="col s12">
+				<h3>ABOUT US</h3>
+				<hr class='stat-hr'>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col s12">
+			<h2>GUIDED BY OUR CORE VALUES</h2>
+			<p>
+				WE PROVIDE AN OUTLET TO HELP ATHLETES
+				EVOLVE AND EXCEL IN LIFE THROUGH
+				THE GAME OF BASKETBALL.
+			</p>
+			<div class="btnContainer">
+				<a href="#" class="btn stat-btn--orange">MORE ABOUT US <i class="material-icons right">arrow_forward</i></a>
+			</div>
+		</div>
+
+	</div>
+
+</section>
+
+
+<section id="quoteSection">
+	<div class="row">
+		<div class="col s12">
+			<div class="quotesection--hero">
+						<img class="jumbo-image" src="./wp-content/themes/stat-basketball/img/DSC_1325.jpg">
+						<img class="quote" src="./wp-content/themes/stat-basketball/img/quote.png">
+						<div class="headline-container">
+								<h2>
+									AT STAT, EVERYBODY HAS TO BE
+									WILLING TO GIVE UP THEIR PERSONAL GOALS,
+									TO COME TOGETHER FOR ONE GOAL TO ACHIEVE.
+								</h2>
+								<h4>ISAIAH ROBERTSON</h4>
+								<h5>FOUNDER</h5>
+								<hr class='stat-hr'>
+
+						</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="upcomingEventsSection">
+	<div class="row">
+		<div class="col s12">
+			<div class="upcomingEvents--hero">
+					<h3>NEXT GROUP SESSION</h3>
+					<h2>OCTOBER 11TH, 2016</h2>
+					<h4>12 - 2PM | ROBINSON CENTER | RICHMOND, VA</h4>
+					<div class="btnContainer">
+						<a href="#" class="btn stat-btn--black">REGISTER NOW <i class="material-icons right">arrow_forward</i></a>
+					</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="recentPosts">
+	<div class="row">
+		<div class="col s12">
+				<h3>RECENT POSTS</h3>
+				<hr class='stat-hr'>
+		</div>
+		<div class="col s12">
+			<div class="col s12 l6">
+				<div class="col s12 featured-post" style="background:linear-gradient(rgba(01,01,01,0.9),rgba(01,01,01,0.9)),url(./wp-content/themes/stat-basketball/img/DSC_1215.jpg);background-size:cover;background-repeat:no-repeat">
+					<h4>THE LOVE OF THE GAME | #STATthat 001</h4>
+					<div class="btnContainer">
+						<a href="#" class="btn stat-btn--orange">VIEW POST <i class="material-icons right">arrow_forward</i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 l6">
+				<div class="col s12 featured-post" style="background:linear-gradient(rgba(01,01,01,0.9),rgba(01,01,01,0.9)),url(./wp-content/themes/stat-basketball/img/DSC_1215.jpg);background-size:cover;background-repeat:no-repeat">
+					<h4>THE LOVE OF THE GAME | #STATthat 001</h4>
+					<div class="btnContainer">
+						<a href="#" class="btn stat-btn--orange">VIEW POST <i class="material-icons right">arrow_forward</i></a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
 
 
 
